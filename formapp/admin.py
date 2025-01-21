67dec3de92f09a694629e4993fd9106537904cbc
Diogo Rodrigues <diogosouzak15@gmail.com>
2025-01-21 20:48:30 -0300

    Implementado os models Base, UserSystem, criado a pagina form html, registrado o model da classe UserSystem no admin.py, e criado outros diretorios

--- formapp/admin.py
+++ formapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserSystem
+
+
+@admin.register(UserSystem)
+class UserSystemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'create', 'modified', 'active')
--- formapp/models.py
+++ formapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    create = models.DateField('Data de criação', auto_now_add=True)
+    modified = models.DateField('Data de atualização', auto_now=True)
+    active = models.BooleanField('Ativo?', default=True)
+
+
+class UserSystem(Base):
+    name = models.CharField('Nome', max_length=255)
+    surname = models.CharField('Sobrenome', max_length=255)
+    email = models.EmailField('E-mail', max_length=100)
+    password = models.CharField('Senha', max_length=255, editable=False)
+
+    class Meta:
+        db_table = 'Usuários'
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
